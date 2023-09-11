e7682e6a79db4ef7a65bba51879d0730742a316f
Gareth Trufitt <gtrufitt@gmail.com>
2023-09-11 17:31:47 +0100

    Don't fail on no hacker news article

--- index.js
+++ index.js
@@ -45,28 +45,25 @@ app.post('/scrape-and-summarize', async (req, res) => {
         // Step 3: Search for the URL in Hacker News and get a summary of the comments
         // Step 2: Search for the URL in Hacker News to get the story ID
         const hnSearchResponse = await axios.get(`http://hn.algolia.com/api/v1/search?query=${encodeURIComponent(url)}&tags=story`);
+        let hackerNewsCommentsSummary;
+        if (hnSearchResponse.data.hits.length !== 0) {
+            // Get the story ID
+            const storyID = hnSearchResponse.data.hits[0].objectID;
 
-        if (hnSearchResponse.data.hits.length === 0) {
-            return res.status(404).send('Story not found on Hacker News');
+            // Step 3: Get all comments for the story
+            const hnCommentsResponse = await axios.get(`http://hn.algolia.com/api/v1/search?tags=comment,story_${storyID}`);
+            const comments = hnCommentsResponse.data.hits.map(hit => hit.comment_text);
+            const limitedComments = comments.slice(0, 25);
+
+            // Step 4: Get a summary of the comments
+            const openAiResponseComments = await openai.chat.completions.create({
+                model: 'gpt-4',
+                messages: limitedComments.map(comment => ({ role: 'user', content: `Here are a number of comments on an article summarised as ${summary}, please provide a summary of the comments below (in no more than 2 sentences): ${comment}` })),
+                stream: false,
+            });
+
+            hackerNewsCommentsSummary = openAiResponseComments.choices[0].message.content;
         }
-
-        // Get the story ID
-        const storyID = hnSearchResponse.data.hits[0].objectID;
-
-        // Step 3: Get all comments for the story
-        const hnCommentsResponse = await axios.get(`http://hn.algolia.com/api/v1/search?tags=comment,story_${storyID}`);
-        const comments = hnCommentsResponse.data.hits.map(hit => hit.comment_text);
-        const limitedComments = comments.slice(0, 25);
-
-        // Step 4: Get a summary of the comments
-        const openAiResponseComments = await openai.chat.completions.create({
-            model: 'gpt-4',
-            messages: limitedComments.map(comment => ({ role: 'user', content: `Here are a number of comments on an article summarised as ${summary}, please provide a summary of the comments below (in no more than 2 sentences): ${comment}` })),
-            stream: false,
-        });
-
-        const hackerNewsCommentsSummary = openAiResponseComments.choices[0].message.content;
-
         // Step 4: Create a markdown representation
         const markdownData = `
 
